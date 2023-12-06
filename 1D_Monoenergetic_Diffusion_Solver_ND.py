2a53c7363b843d16719e94115be2f57d6a05273e
Nic Daneman <93825546+nicdane@users.noreply.github.com>
2023-12-05 22:18:49 -0500

    Add files via upload

---
+++ 1D_Monoenergetic_Diffusion_Solver_ND.py
@@ -0,0 +1,190 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Constants
+D = 1.0        # Diffusion coefficient
+SIGMA_A = 0.6  # Absorption cross section for left material
+SIGMA_F = 0.6  # Fission cross section
+SIGMA_B = 1.5  # Absorption cross section for the right material
+material_boundary = 5  # Index where the material changes
+NU = 2.50      # Average number of neutrons produced per fission
+L = 100        # total length of cells
+N = 9         # Number of discretization points (basically resolution)
+h = L / N      # Mesh spacing
+S = 1.0        #Source strength
+
+# Function to create A matrix (physical system)
+def create_matrix_A(N, D, SIGMA_A, SIGMA_B, material_boundary, h):
+    A = np.zeros((N, N))
+
+    for i in range(N):
+        # Determine the absorption cross section for the current point
+        sigma = SIGMA_A * S if i < material_boundary else SIGMA_B
+
+        if i == 0:
+            # Reflecting boundary at the left
+            A[i, i] = 2 * D / h**2 + sigma
+            A[i, i + 1] = -2 * D / h**2
+        elif i == N - 1:
+            # Vacuum boundary at the right
+            A[i, i] = -D / h** + sigma #not sure if this sigma is right but it helped fix some vacuum boundary matrix issues
+            A[i, i - 1] = 2 * D / h**2 
+        else:
+            # Internal cells
+            A[i, i - 1] = -D / h**2
+            A[i, i] = 2 * D / h**2 + sigma
+            A[i, i + 1] = -D / h**2
+
+    return A
+
+
+# Function to create the F matrix using fission cross section vals
+def create_matrix_F(N, SIGMA_F, NU):
+    F = np.diag(np.full(N, NU * SIGMA_F))
+    return F
+
+#iterative solver to converge flux + k values
+#returns 1/k to be converted
+def gauss_seidel_eigenvalue(A, F, tol=1e-4, max_iter=100):
+    # Initial guess for the flux vector phi and eigenvalue lambda (1/k)
+    phi = np.ones(N)
+    lambda_old = 0.5
+
+    for _ in range(max_iter):
+        phi_old = phi.copy()
+
+        # Gauss-Seidel iteration for A*phi = lambda*F*phi
+        for i in range(N):
+            sigma = np.dot(A[i, :i], phi[:i]) + np.dot(A[i, i+1:], phi_old[i+1:])
+            phi[i] = (lambda_old * np.dot(F[i], phi_old)) / A[i, i]
+
+        # Normalize phi
+        phi /= np.linalg.norm(phi)
+
+        # Update lambda (1/k)
+        lambda_new = np.dot(phi, np.dot(F, phi)) / np.dot(phi, np.dot(A, phi))
+
+        # Convergence check
+        if np.linalg.norm(phi - phi_old, ord=np.inf) < tol:
+            return 1/lambda_new, phi
+
+        lambda_old = lambda_new
+
+    return 1/lambda_new, phi
+
+# Using inverted matrix method to quickly see flux vals
+#NB: Some plots resulting from this subroutine were used during presentation. Kept subroutine still intact as it's still 'most' accurate
+def eigenvalue_test(A, F):
+
+    phi = np.ones(N)
+    F_inv = np.linalg.inv(F)
+    eigenvalue_matrix = F_inv.dot(A)
+
+    # calculate eigenvalues
+    eigenvalues, eigenvectors = np.linalg.eig(eigenvalue_matrix)
+
+    return eigenvalues, eigenvectors
+
+# helpful for quickly visualizing 1D system without having to closely inspect numbers
+def plot_matrix_heatmap(matrix, title='Matrix Heatmap'):
+
+    plt.figure(figsize=(8, 6))
+    plt.imshow(matrix, cmap='hot', interpolation='nearest')
+    plt.colorbar()
+    plt.title(title)
+    plt.xlabel('Column Index')
+    plt.ylabel('Row Index')
+    plt.show()
+
+#again this is an older subroutine but my gauss seidel method appears to be performing worse as opposed to inverse method
+def plot_flux_distributions(eigenvectors, N):
+
+    x = range(N)  # Discretization points
+
+    for i in range(eigenvectors.shape[1]):
+        "toggle all eigenvectors or just one by commenting out one or the other below"
+        #plt.plot(x, eigenvectors[:, i], label=f'Eigenvalue {i+1}')
+        plt.plot(x, eigenvectors[:, 0]) #Here I'm just plotting the first one -- distribution likely faulty
+
+    plt.xlabel('Discretization Points')
+    plt.ylabel('Neutron Flux')
+    plt.title('Neutron Flux Distributions')
+    plt.legend()
+    plt.show()
+
+#MAIN ROUTINE
+
+# Creating matrices A and F
+A = create_matrix_A(N, D, SIGMA_A, SIGMA_B, material_boundary, h)
+F = create_matrix_F(N, SIGMA_F, NU)
+
+# Solving for eigenvalue
+eigenvalues, eigenvectors = eigenvalue_test(A, F)
+
+solution = gauss_seidel_eigenvalue(A, F)
+print("\n ",solution, "\n") #first output here -- these are directly correlating to A matrix -- something went wrong
+
+# Printing the results for matrices A and F
+print(A)
+print(F)
+
+# After solving for eigenvalues and eigenvectors
+plot_flux_distributions(eigenvectors, N)
+print('\n \n')
+
+# Main execution
+A = create_matrix_A(N, D, SIGMA_A, SIGMA_B, material_boundary, h)
+F = create_matrix_F(N, SIGMA_F, NU)
+
+k, flux = gauss_seidel_eigenvalue(A, F)
+
+print(f"Converged eigenvalue (1/k): {k}")
+print(f"Flux distribution: {flux}")
+print('\n \n \n')
+
+#This will pop up after you close the flux plot. Bottom cell second from right vacuum appears to still be faulty in A matrix creation
+plot_matrix_heatmap(A, title='Heatmap of Matrix A')
+
+#EXTRA STUFF
+# Warning >> Power Method is very broken. You will get a 0 value K...NOT GOOD! Terrible neutron economy >__<
+"""def power_method(A, F, tol=1e-4, max_iter=100):
+    N = len(A)  
+    phi = np.ones(N)  # Initial guess for the flux
+    k_old = 1.0  # Initial guess for the eigenvalue -- shouldn't matter but 1.0 is standard
+
+    for _ in range(max_iter):
+        # Calculate F * phi
+        F_phi = np.dot(F, phi)
+
+        # Solve A * new_phi = F * phi
+        new_phi = gauss_seidel(A, F_phi)
+
+        # Normalize new_phi and prevent overflow
+        norm = np.linalg.norm(new_phi)
+        if norm == 0 or np.isinf(norm) or np.isnan(norm):
+            break  # Break if norm is zero, infinity, or nan
+        new_phi /= norm
+
+        # some checks for weird cases (infinity or nan values to break method -- for some situations the code just won't exit
+        k_new = k_old * norm
+        if np.isinf(k_new) or np.isnan(k_new):
+            break  # Break if k_new is infinity or NaN
+
+        # Check for convergence
+        if np.abs(k_new - k_old) / np.abs(k_old) < tol:
+            return 1 / k_new, new_phi
+
+        phi = new_phi
+        k_old = k_new
+
+    return 1 / k_new, phi
+
+# Main execution using Power Method
+A = create_matrix_A(N, D, SIGMA_A, SIGMA_B, material_boundary, h)
+F = create_matrix_F(N, SIGMA_F, NU)
+
+k, flux = power_method(A, F)
+
+print(f"Converged eigenvalue (1/k): {k}")
+print(f"Flux distribution: {flux}")
+plt.plot(flux)"""
